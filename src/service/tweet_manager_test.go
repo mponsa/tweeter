17bf36dcf584ead2bf66189e9dc6a71e160dcb9e
mponsa <manuponsa@gmail.com>
2019-08-20 15:39:29 -0300

    hollala

--- src/service/tweet_manager_test.go
+++ src/service/tweet_manager_test.go
@@ -1,2 +1,17 @@
 package service_test
 
+import (
+	"github.com/mponsa/tweeter/src/service"
+	"testing"
+)
+
+func TestPublishedTweetIsSaved(t *testing.T){
+	var tweet string = "My first tweet"
+
+	service.PublishTweet(tweet)
+
+	if service.GetTweet() != tweet {
+		t.Error("Expected tweet is",tweet)
+	}
+}
+
